93f97e8fbd4dfe52d262b9379332128e7360cd35
slacker151 <alec940123@live.com>
2016-04-04 20:57:31 -0400

    update

--- 04_Aprl/2016.Aprl.04/README.md
+++ 04_Aprl/2016.Aprl.04/README.md
@@ -1,7 +1,24 @@
 Problem Description:
 
-	
+	Breadth First Traversal for a graph.
+
+	Breadth first travesal for a graph is similar to breadth first 
+	traversal of a tree. The only catch here is, unlike trees, 
+	graphs may contain cycles, so we may come to the same node 
+	again. To avoid processing a node more than onec, we use a 
+	boolean visited array. For simplicity, it is assumes that all 
+	vertices are reachable from the starting vertex. Breadth, 
+	meaning the graph is traversed level by level.
 
 Problem Solution:
 
+	Given a vertex in the graph, determine the BFT. Follow the 	
+	following procedures:
+	
+	1. Create a bool[] with size of NumOfVertices
+	2. Create a queue list and push the starting point in
+	3. While the queue is not empty, pop and display the vertice
+	4. If the vertice has never been visited from the bool[] then,
+	5. For each vertice, push its linked vertices into the queue
+	
 	
--- 04_Aprl/2016.Aprl.04/main.cpp
+++ 04_Aprl/2016.Aprl.04/main.cpp
@@ -3,9 +3,84 @@
 #include <cstdlib>
 #include <iomanip>
 #include <string>
+#include <list>
+
+class Graph
+{
+public:
+	Graph(int);
+	~Graph();
+	void addEdge(int v, int w);
+	void BFS(int s);
+private:
+	int V;
+	std::list<int> *adj;
+};
+
+Graph::Graph(int NumOfVertices)
+	: V(0)
+	, adj(NULL)
+{
+	this->V = NumOfVertices;
+	adj = new std::list<int>[NumOfVertices];
+}
+
+Graph::~Graph()
+{
+	if(adj != NULL)
+	{
+		delete [] adj;
+	}
+}
+
+void Graph::addEdge(int v, int w)
+{
+	adj[v].push_back(w);
+}
+
+void Graph::BFS(int start)
+{
+	bool *visited = new bool[V];
+	std::fill_n(visited, V, false);
+
+	std::list<int> queue;
+
+	visited[start] = true;
+	queue.push_back(start);
+
+	while(!queue.empty())
+	{
+		int temp = queue.front();
+		std::cout << std::setw(4) << temp;
+		queue.pop_front();
+
+		std::list<int>::iterator it;
+
+		for (it = adj[temp].begin(); it != adj[temp].end(); ++it)
+		{
+			if(!visited[*it])
+			{
+				visited[*it] = true;
+				queue.push_back(*it);
+			}
+		}
+	}
+	std::cout << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
-	
+	Graph G(4);
+	G.addEdge(0, 1);
+	G.addEdge(0, 2);
+	G.addEdge(1, 2);
+	G.addEdge(2, 0);
+	G.addEdge(2, 3);
+	G.addEdge(3, 3);
+
+	std::cout << "Following is BFT (starting from vertex 2): \n"; 
+
+	G.BFS(2);
+
 	return 0;
 }
